da70b577e1deaa861554ba281bcb3808df08578a
avinashpingale <avinashpingale@gmail.com>
2022-01-15 13:08:06 +0530

    Everything is checked

--- src/test/java/com/resonance/traveltests/Travel.java
+++ src/test/java/com/resonance/traveltests/Travel.java
@@ -35,4 +35,9 @@ public class Travel extends BaseFramework{
 	public void verifySearchResultsForShoes() {
 		enterText(Locator.homeScreen_searchTxtBx,"Shoes");
 	}
+	
+	@Test
+	public void m1() {
+		System.out.println("My Test case");
+	}
 }
